005bcdadee2f0fa35eb8ffe19dac542730173d22
Dearshor Xue <dearshor@gmail.com>
2012-09-21 06:30:03 +0800

    添加了基于servlet 3.0的spring dispatch servlet config
    Signed-off-by: Dearshor Xue <dearshor@gmail.com>

---
+++ src/main/java/com/kjlink/quartz/manager/config/DispatcherServletInitializer.java
@@ -0,0 +1,24 @@
+package com.kjlink.quartz.manager.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+
+public class DispatcherServletInitializer implements WebApplicationInitializer {
+
+	public void onStartup(ServletContext servletContext)
+			throws ServletException {
+		AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+		appContext.register(AppConfig.class);
+
+		Dynamic servlet = servletContext.addServlet("main", new DispatcherServlet(appContext));
+		servlet.addMapping("/");
+		servlet.setAsyncSupported(true);
+	}
+
+}

ea9dda762f164e165cf28be673c352dae8c117d1
Dearshor Xue <dearshor@gmail.com>
2012-10-18 17:15:13 +0800

    hacking...
    Signed-off-by: Dearshor Xue <dearshor@gmail.com>

--- src/main/java/com/kjlink/quartz/manager/config/AppConfigQuartz.java
+++ src/main/java/com/kjlink/quartz/manager/config/AppConfigQuartz.java
@@ -2,15 +2,13 @@ package com.kjlink.quartz.manager.config;
 
 import javax.sql.DataSource;
 
-import org.quartz.JobDataMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.scheduling.quartz.JobDetailBean;
+import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
-import com.kjlink.quartz.manager.jobs.MyQuartzJobBean;
 import com.kjlink.quartz.manager.service.SimpleService;
 
 @Configuration
@@ -22,12 +20,11 @@ public class AppConfigQuartz {
 	@Autowired
 	private DataSource dataSource;
 
-	public @Bean JobDetailBean jobDetail() {
-		JobDetailBean jobDetailBean = new JobDetailBean();
-		jobDetailBean.setJobClass(MyQuartzJobBean.class);
-		JobDataMap jobDataAsMap = new JobDataMap();
-		jobDataAsMap.put("simpleService", simpleService);
-		jobDetailBean.setJobDataAsMap(jobDataAsMap);
+	public @Bean MethodInvokingJobDetailFactoryBean jobDetail() {
+		MethodInvokingJobDetailFactoryBean jobDetailBean = new MethodInvokingJobDetailFactoryBean();
+		jobDetailBean.setTargetObject(simpleService);
+		jobDetailBean.setTargetMethod("testMethod");
+		jobDetailBean.setConcurrent(false);
 		return jobDetailBean;
 	}
 	
@@ -36,6 +33,7 @@ public class AppConfigQuartz {
 		schedulerFactoryBean.setDataSource(dataSource);
 		schedulerFactoryBean.setApplicationContextSchedulerContextKey("applicationContextKey");
 		schedulerFactoryBean.setConfigLocation(new ClassPathResource("quartz.properties"));
+		schedulerFactoryBean.setJobFactory(jobDetail());
 		return schedulerFactoryBean;
 	}
 
--- src/main/java/com/kjlink/quartz/manager/controller/JobController.java
+++ src/main/java/com/kjlink/quartz/manager/controller/JobController.java
@@ -1,22 +1,20 @@
 package com.kjlink.quartz.manager.controller;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import org.quartz.JobDetail;
+import org.quartz.JobKey;
 import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
 import org.quartz.Trigger;
+import org.quartz.TriggerKey;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.condition.ConsumesRequestCondition;
 
 @Controller
 @RequestMapping("/schedule/")
@@ -30,33 +28,55 @@ public class JobController {
 
 	@RequestMapping(value = "add", method = RequestMethod.POST)
 	public void add() {
-		scheduler.addJob(jobDetail, true);
+		try {
+			scheduler.addJob(jobDetail, true);
+		} catch (SchedulerException e) {
+			// TODO Auto-generated catch block
+			throw new IllegalStateException(e);
+		}
 	}
 
 	@RequestMapping(value = "query/{jobGroup}/{jobName}", method = RequestMethod.GET, consumes = "application/json")
 	@ResponseBody
-	public List<Trigger> query(@PathVariable String jobName, @PathVariable String jobGroup) {
-		Trigger[] triggersOfJob = scheduler.getTriggersOfJob(
-				jobName, jobGroup);
-		List<Trigger> trigList = Arrays.asList(triggersOfJob);
-		return trigList;
+	public List<? extends Trigger> query(@PathVariable String jobName, @PathVariable String jobGroup) {
+		List<? extends Trigger> triggersOfJob;
+		try {
+			triggersOfJob = scheduler.getTriggersOfJob(
+					new JobKey(jobName, jobGroup));
+		} catch (SchedulerException e) {
+			throw new IllegalStateException(e);
+		}
+		return triggersOfJob;
 	}
 
 	@RequestMapping(value = "pause/{groupName}/{triggerName}", method = RequestMethod.GET)
 	public void pause(@PathVariable String groupName, @PathVariable String triggerName) {
-		scheduler.pauseTrigger(triggerName, groupName);
+		try {
+			scheduler.pauseTrigger(new TriggerKey(triggerName, groupName));
+		} catch (SchedulerException e) {
+			throw new IllegalStateException(e);
+		}
 	}
 
 	@RequestMapping(value = "resume/{groupName}/{triggerName}", method = RequestMethod.GET)
 	public void resume(@PathVariable String groupName, @PathVariable String triggerName) {
-		// TODO Auto-generated method stub
-		scheduler.resumeTrigger(triggerName, groupName);
+		try {
+			scheduler.resumeTrigger(new TriggerKey( triggerName, groupName));
+		} catch (SchedulerException e) {
+			// TODO Auto-generated catch block
+			throw new IllegalStateException(e);
+		}
 	}
 
 	@RequestMapping(value = "remove/{groupName}/{triggerName}", method = RequestMethod.GET)
 	public void remove(@PathVariable String groupName, @PathVariable String triggerName) {
 		// TODO Auto-generated method stub
-		scheduler.unscheduleJob(triggerName, groupName);
+		try {
+			scheduler.unscheduleJob(new TriggerKey( triggerName, groupName));
+		} catch (SchedulerException e) {
+			// TODO Auto-generated catch block
+			throw new IllegalStateException(e);
+		}
 	}
 
 }

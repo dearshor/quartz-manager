238d29760e4a8abc46ec4c84d168ce33e19ef7e6
Dearshor Xue <dearshor@gmail.com>
2012-10-19 13:46:22 +0800

    hacking...
    Signed-off-by: Dearshor Xue <dearshor@gmail.com>

--- src/main/java/com/kjlink/quartz/manager/config/AppConfigQuartz.java
+++ src/main/java/com/kjlink/quartz/manager/config/AppConfigQuartz.java
@@ -2,10 +2,13 @@ package com.kjlink.quartz.manager.config;
 
 import javax.sql.DataSource;
 
+import org.quartz.JobDetail;
+import org.quartz.Trigger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.scheduling.quartz.CronTriggerBean;
 import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
@@ -20,12 +23,18 @@ public class AppConfigQuartz {
 	@Autowired
 	private DataSource dataSource;
 
-	public @Bean MethodInvokingJobDetailFactoryBean jobDetail() {
+	public @Bean JobDetail jobDetail() {
 		MethodInvokingJobDetailFactoryBean jobDetailBean = new MethodInvokingJobDetailFactoryBean();
 		jobDetailBean.setTargetObject(simpleService);
 		jobDetailBean.setTargetMethod("testMethod");
 		jobDetailBean.setConcurrent(false);
-		return jobDetailBean;
+		return jobDetailBean.getObject();
+	}
+	
+	public @Bean CronTriggerBean cronTrigger() {
+		CronTriggerBean cronTriggerBean = new CronTriggerBean();
+		cronTriggerBean.
+		return cronTriggerBean;
 	}
 	
 	public @Bean(name = "quartzScheduler")  SchedulerFactoryBean schedulerFactoryBean() {
@@ -33,7 +42,6 @@ public class AppConfigQuartz {
 		schedulerFactoryBean.setDataSource(dataSource);
 		schedulerFactoryBean.setApplicationContextSchedulerContextKey("applicationContextKey");
 		schedulerFactoryBean.setConfigLocation(new ClassPathResource("quartz.properties"));
-		schedulerFactoryBean.setJobFactory(jobDetail());
 		return schedulerFactoryBean;
 	}
 
